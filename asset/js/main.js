6c1cbe19e0c9d9029cff291dd5473bfe4ada11dd
mathieudebavelaere <mathieu.debavelaere@hotmail.com>
2022-11-09 16:43:27 +0100

    cv numerique 9nov

---
+++ asset/js/main.js
@@ -0,0 +1,25 @@
+/*--------------AnimationAccueil--------------*/
+function fadeOutEffect() {
+  var fadeTarget = document.getElementById("popup");
+  var fadeEffect = setInterval(function () {
+      if (!fadeTarget.style.opacity) {
+          fadeTarget.style.opacity = 1;
+      }
+      if (fadeTarget.style.opacity > 0) {
+          fadeTarget.style.opacity -= 0.1;
+      } else {
+          clearInterval(fadeEffect);
+      }
+  }, 200);
+}
+cercle.addEventListener("click",()=>{
+  fadeOutEffect;
+  overlay.classList.remove("blur-in")
+  overlay.classList.add("blur-out")
+  popup.classList.remove("shadow")
+  popup.classList.add("lol")
+setTimeout(()=>{
+  popup.style.display="none"},2000)
+})
+/*--------------FinD'AnimationAccueil--------------*/
+
